edc6f5cb0709ba68e8dc98749a0f57dc60113173
bvanderhorn <bvanderhorn@gmail.com>
2023-05-02 13:10:32 +0200

    feat: day 22 part 1

--- src/2/2.ts
+++ src/2/2.ts
@@ -1,3 +1,5 @@
 import * as h from '../helpers';
+var occurs = (s: string, sub:string) : number => (s.match(new RegExp(sub, "g")) || []).length;
 var passwords = h.read(2, "passwords.txt").match(/(\d+)\-(\d+)\s+(\w+)\:\s+(\w+)/);
-h.print(passwords[0]);
+var valid = passwords.map(pw => +(occurs(pw[4], pw[3]) >= pw[1] && occurs(pw[4], pw[3]) <= pw[2])).sum();
+h.print(valid);
