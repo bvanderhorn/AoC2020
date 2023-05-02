69082453e8f2c3e27eb1340b7ceb6b88f5fe78b7
bvanderhorn <bvanderhorn@gmail.com>
2023-05-02 21:04:42 +0200

    feat: cleanup

--- src/5/5.ts
+++ src/5/5.ts
@@ -1,4 +1,4 @@
 import * as h from '../helpers';
-var getId = (s: string) : number => h.bin2dec(s.replace(/F|L/g, "0").replace(/B|R/g, "1"), 2);
+var getId = (s: string) : number => h.bin2dec(s.replace(/F|L/g, "0").replace(/B|R/g, "1"));
 var seats = h.read(5, "seats.txt");
 h.print("part 1: " + seats.map(getId).max());
