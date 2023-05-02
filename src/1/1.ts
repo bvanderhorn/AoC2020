b592697f084e1b62edc131c2ed89a6e1d17ed0ad
bvanderhorn <bvanderhorn@gmail.com>
2023-05-02 12:33:33 +0200

    feat: day 1 part 1

--- src/1/1.ts
+++ src/1/1.ts
@@ -1,3 +1,3 @@
 import * as h from '../helpers';
-var expenses = h.read(1, "expenses.txt");
-h.print(expenses.slice(0,10));
+var expenses = h.read(1, "expenses.txt").tonum();
+expenses.map((e1, i1) => expenses.slice(i1+1).map((e2, _) => e1 + e2 == 2020 ? h.print(e1 * e2) : null));
