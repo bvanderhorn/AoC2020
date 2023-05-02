cda9cafbcaa206c9e817d6bcc65d8bfac9c01419
bvanderhorn <bvanderhorn@gmail.com>
2023-05-02 21:08:17 +0200

    feat: day 5 part 2

--- src/5/5.ts
+++ src/5/5.ts
@@ -1,4 +1,6 @@
 import * as h from '../helpers';
 var getId = (s: string) : number => h.bin2dec(s.replace(/F|L/g, "0").replace(/B|R/g, "1"));
 var seats = h.read(5, "seats.txt");
-h.print("part 1: " + seats.map(getId).max());
+var ids = seats.map(getId);
+h.print("part 1: " + ids.max());
+h.print("part 2: " +  (ids.filter(s => !ids.includes(s+1) && ids.includes(s+2))[0]+1));
