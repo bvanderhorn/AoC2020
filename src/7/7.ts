e27c3924a54b3a1bdfa2680cb74df248ece47b1f
bvanderhorn <bvanderhorn@gmail.com>
2023-05-03 14:26:42 +0200

    feat: day 7 part 1

--- src/7/7.ts
+++ src/7/7.ts
@@ -4,3 +4,13 @@ var getContained = (rule: string[][][]) : string[] => rule[1].map((r) => r[1]);
 var rules = h.read(7, "rules.txt").split(/contains?/).split(",").replace(/bag.*$/,'').trim().match(/(\d+)?\s*(.*)\s*/, true);
 h.print(h.stringify(rules.slice(0,2)));
 h.print("containing: " ,getContaining(rules[1]), "contained: " ,getContained(rules[1]));
+var checkBag = "shiny gold";
+var contained: string[] = [];
+var remaining = [checkBag];
+while (remaining.length > 0 ) {
+    var current = remaining.shift();
+    var containing = rules.filter((r) => getContained(r).includes(current!)).map((r) => getContaining(r));
+    contained = contained.concat(containing);
+    remaining = remaining.concat(containing);
+}
+h.print("part 1: ", contained.unique().length);
