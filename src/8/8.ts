60f57e9f38e33727a81e18d92ad723f326204886
bvanderhorn <bvanderhorn@gmail.com>
2023-05-03 16:55:40 +0200

    feat: day 8 part 2

--- src/8/8.ts
+++ src/8/8.ts
@@ -12,7 +12,6 @@ var doRun = (instructions: string[][]) : State => {
     while (!visited.includes(i) && i < instructions.length) {
         visited.push(i);
         var instruction = instructions[i];
-        // h.print(counter,": instruction ",i,": ", instruction);
         value += instruction[0] == "acc" ? +instruction[1] : 0;
         i += instruction[0] == "jmp" ? +instruction[1] : 1;
     }
@@ -30,11 +29,16 @@ h.print("part 1: ", state1.value, " (after ",state1.visited.length," instruction
 
 var possibleFaultyInstructions = state1.visited.filter(i => ["jmp", "nop"].includes(instructions[i][0]));
 h.print("possible faulty instructions: ", possibleFaultyInstructions.length);
+var state2: State;
+var faultyInstruction = -1;
 for (const i of possibleFaultyInstructions) {
-    h.print("trying to fix instruction ", i, " (", instructions[i], ")");
-    var newInstructions = instructions.slice();
+    var newInstructions = instructions.copy();
     newInstructions[i][0] = newInstructions[i][0] == "jmp" ? "nop" : "jmp";
-    var state2 = doRun(newInstructions);
-    h.print("   valid: ", state2.valid,", value: ", state2.value,", reason: ", state2.reason, ", (after ",state2.visited.length," instructions)");
-    if (state2.valid) break;
+    state2 = doRun(newInstructions);
+    // h.print("fixing instruction ",i," => valid: ", state2.valid,", value: ", state2.value,", reason: ", state2.reason, ", (after ",state2.visited.length," instructions)");
+    if (state2.valid) {
+        faultyInstruction = i;
+        break;
+    }
 }
+h.print("part 2: ", state2!.value, " (fixed instruction ",faultyInstruction,", after ", state2!.visited.length," instructions)");
