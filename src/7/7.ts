8f3bbe970134adc33337c19cf9f077c11dab72c3
bvanderhorn <bvanderhorn@gmail.com>
2023-05-03 14:39:40 +0200

    feat: progress

--- src/7/7.ts
+++ src/7/7.ts
@@ -1,16 +1,29 @@
 import * as h from '../helpers';
 var getContaining = (rule: string[][][]) : string => rule[0][0][1];
-var getContained = (rule: string[][][]) : string[] => rule[1].map((r) => r[1]);
+var getContained = (rule: string[][][]) : string[][] => rule[1].filter((r) => r[1] != "no other");
 var rules = h.read(7, "rules.txt").split(/contains?/).split(",").replace(/bag.*$/,'').trim().match(/(\d+)?\s*(.*)\s*/, true);
 h.print(h.stringify(rules.slice(0,2)));
 h.print("containing: " ,getContaining(rules[1]), "contained: " ,getContained(rules[1]));
 var checkBag = "shiny gold";
-var contained: string[] = [];
+
+// part 1
+var containing: string[] = [];
 var remaining = [checkBag];
 while (remaining.length > 0 ) {
     var current = remaining.shift();
-    var containing = rules.filter((r) => getContained(r).includes(current!)).map((r) => getContaining(r));
-    contained = contained.concat(containing);
-    remaining = remaining.concat(containing);
+    var containingCurrent = rules.filter((r) => getContained(r).map(c => c[1]).includes(current!)).map((r) => getContaining(r));
+    containing = containing.concat(containingCurrent);
+    remaining = remaining.concat(containingCurrent);
 }
-h.print("part 1: ", contained.unique().length);
+h.print("part 1: ", containing.unique().length);
+
+// part 2
+var contained : number = 0;
+var remaining = [checkBag];
+while (remaining.length > 0 ) {
+    var current = remaining.shift();
+    var containedCurrent = rules.filter((r) => getContaining(r) == current).map((r) => getContained(r))[0];
+    contained += containedCurrent.map((r) => parseInt(r[0])).sum();
+    remaining = remaining.concat(containedCurrent.map((r) => r[1]));
+}
+h.print("part 2: ", contained);
