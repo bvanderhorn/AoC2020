160da31fac27918385e4e45437651c4fa2bced2f
bvanderhorn <bvanderhorn@gmail.com>
2023-05-04 18:06:59 +0200

    feat: day 11 part 1

--- src/11/11.ts
+++ src/11/11.ts
@@ -1,13 +1,21 @@
 import * as h from '../helpers';
 var printSeats = (seats: number[][], sub: number[] | number = 0) => seats.mape(s => s == 0 ? "L" : s == 1 ? "#" : ".").printc(t => t == "#", "r", "", "\n", sub);
-var seats: number[][] = h.read(11, "seats.txt", "ex").split('').mape(s => s == "L" ? 0 : s == "#" ? 1 : -1);
-printSeats(seats, [10, 10]);
+var seats: number[][] = h.read(11, "seats.txt").split('').mape(s => s == "L" ? 0 : s == "#" ? 1 : -1);
 
 // part 1
 var getSeatedAdjacent = (seats: number[][], y: number, x: number) : number => 
-    h.getnb([y,x], seats.length, seats[0].length, '8').map(p => seats[p[0]][p[1]]).filter(s => s == 1).length;
-var applyMove = (seats: number[][]) : number[][] =>  seats.mapij((y,x,s) => {
+    h.getnb([y,x], seats.length-1, seats[0].length-1, '8').map(p => seats[p[0]][p[1]]).filter(s => s == 1).length;
+var applyMove = (seats: number[][]) : number[][] =>  seats.mapij((y,x,s:number) => {
         var adj = getSeatedAdjacent(seats, y,x);
-        return s == 0 && adj == 0 ? 1 : s == 1 && adj >= 4 ? 0 : s;
+        return (s == 0 && adj == 0) ? 1 : (s == 1 && adj >= 4) ? 0 : s;
     });
-printSeats(applyMove(seats), [10, 10]);
+var oldSeats = seats;
+var newSeats = applyMove(seats);
+var moves = 1;
+while (!h.equals2(oldSeats, newSeats)) {
+    moves++;
+    oldSeats = newSeats;
+    newSeats = applyMove(oldSeats);
+}
+printSeats(newSeats);
+h.print("part 1: " + newSeats.flat().filter(s => s == 1).length, " occupied seats after ", moves, " moves");
