3742e89aed08c9942992190a5087fabe66b41e58
bvanderhorn <bvanderhorn@gmail.com>
2023-05-04 10:01:11 +0200

    feat: more progress

--- src/10/10.ts
+++ src/10/10.ts
@@ -2,9 +2,16 @@ import * as h from '../helpers';
 var adapters = h.read(10, "adapters.txt").tonum().sort((a,b) => a-b);
 adapters.unshift(0);
 adapters.push(adapters.last()+3);
-h.print(adapters.printc(a => {
-    var i = adapters.indexOf(a);
-    return i == 0 || adapters[i+1] - a == 3 || a - adapters[i-1] == 3;
-}, "r", ", "));
+
+// part 1
 var diff = adapters.slice(1).map((a,i) => a - adapters[i]);
 h.print("part 1: ",diff.count(1)*diff.count(3));
+
+// part 2
+var isMandatory = (adapters: number[], i: number) => [0, adapters.length -1].includes(i) || adapters[i] - adapters[i-1] == 3 || adapters[i+1] - adapters[i] == 3;
+
+h.print(adapters.printc(a => isMandatory(adapters, adapters.indexOf(a)), "r", ", "));
+var mandatory = h.range(0, adapters.length).filter(i => isMandatory(adapters, i));
+var groupIndices = mandatory.slice(0, mandatory.length -1).filter((m,i) => mandatory[i+1] !== m+1).map(m => [m, mandatory[mandatory.indexOf(m)+1]]);
+var groups = groupIndices.map(g => adapters.slice(g[0], g[1]+1));
+h.print(groups);
