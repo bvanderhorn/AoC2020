199212818f821a8cafa30d4b75eb8ee53e82b93e
bvanderhorn <bvanderhorn@gmail.com>
2023-05-04 18:48:49 +0200

    feat: progress

--- src/12/12.ts
+++ src/12/12.ts
@@ -1,3 +1,15 @@
 import * as h from '../helpers';
+type State = {
+    pos: number[],
+    dir: string
+}
+var act = (state: State, instruction: string[]) : State => {
+    
+}
 var instructions = h.read(12, "instructions.txt").match(/(\w)(\d+)/, true);
 h.print(instructions.slice(0,5));
+
+var dirs = "NESW";
+var vecs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+var dir = "E";
+var pos = [0,0];
