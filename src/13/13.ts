38e2a670cba3ab672a484dd805693aefa994badd
bvanderhorn <bvanderhorn@gmail.com>
2023-05-06 17:17:20 +0200

    feat: generalize to multipliers and reduced multipliers

--- src/13/13.ts
+++ src/13/13.ts
@@ -8,46 +8,40 @@ h.print("part 1: first bus is ", firstBus[0], " in ", firstBus[1], " minutes, an
 
 // part 2
 type Multiplier = {
-    bus: number[],
     Initial: number,
     Interval: number
 }
-var firstXMatches = (bus1: number[], bus2: number[], nofMatches: number) : number[][] => {
+var firstXMatches = (m1: Multiplier, m2: Multiplier, nofMatches: number) : number[][] => {
     var matches : number[][] = [];
     var n1 = 1;
     while(true) {
-        var x = n1*bus1[1] - bus1[0] + bus2[0];
-        if (x % bus2[1] == 0) matches.push([n1, x/bus2[1]]);
+        var x = n1*m1.Interval + m1.Initial - m2.Initial;
+        if (x % m2.Interval == 0) matches.push([n1, x/m2.Interval]);
         if (matches.length >= nofMatches) return matches;
         n1++;
     }
 }
-var checkMatches = (matches: number[][], bus1: number[], bus2: number[]) : void => {
+var checkMatches = (matches: number[][], m1: Multiplier, m2: Multiplier) : void => {
     matches.forEach(m => {
-        var x1 = m[0]*bus1[1] - bus1[0];
-        var x2 = m[1]*bus2[1] - bus2[0];
-        h.print("check match", m, "with buses" , bus1, ",", bus2, ": => ", x1, x2, x1 == x2)
+        var x1 = m[0]*m1.Interval + m1.Initial;
+        var x2 = m[1]*m2.Interval + m2.Initial;
+        h.print("check match", m, "with multipliers" , m1, ",", m2, ": => ", x1, x2, x1 == x2)
     });
 }
-var getAllowedMultipliers = (bus1: number[], bus2: number[]) : Multiplier[] => {
-    var matches = firstXMatches(bus1, bus2, 2);
-    return [0,1].map(i => ({bus: i==0? bus1 : bus2, Initial: matches[0][i], Interval: matches[1][i] - matches[0][i]}));
+var getAllowedMultipliers = (m1: Multiplier, m2: Multiplier) : Multiplier[] => {
+    var matches = firstXMatches(m1, m2, 2);
+    return [0,1].map(i => ({Initial: matches[0][i], Interval: matches[1][i] - matches[0][i]}));
 }
-var getAllMultiplierCouples = (buses: number[][]) : Multiplier[] => {
-    var multipliers : Multiplier[] = [];
-    for (const i of h.range(0, buses.length -1)) multipliers.push(...getAllowedMultipliers(buses[i], buses[i+1]));
-    return multipliers;
+var getAllMultipliersOfFirst = (multipliers: Multiplier[]) : Multiplier[] => {
+    var reducedMultipliers : Multiplier[] = [];
+    for (const i of h.range(1, buses.length)) reducedMultipliers.push(getAllowedMultipliers(multipliers[0], multipliers[i])[0]);
+    return reducedMultipliers;
 }
-var getAllMultipliersOfGivenBus = (buses: number[][], busIndex: number) : Multiplier[] => {
-    var multipliers : Multiplier[] = [];
-    for (const i of h.range(0, buses.length)) if (i != busIndex) multipliers.push(getAllowedMultipliers(buses[busIndex], buses[i])[0]);
-    return multipliers;
-}
-var buses2 : number[][] = rawbuses.map((b,i) => [i,b]).filter(b => b[1] != 'x').map(b => [b[0], +b[1]]).sort((a,b) => b[1] - a[1]);
+var buses2 : Multiplier[] = rawbuses.map((b,i) => [i,b]).filter(b => b[1] != 'x').map(b => [b[0], +b[1]]).sort((a,b) => b[1] - a[1]).map(b => ({Initial: -b[0], Interval: b[1]}));
 h.print(buses2);
 var matches = firstXMatches(buses2[0], buses2[1], 3);
 checkMatches(matches, buses2[0], buses2[1]);
 var deltas = matches.slice(1).map((m,i) => [m[0] - matches[i][0], m[1] - matches[i][1]]);
 h.print(deltas);
 // h.print(h.stringify(getAllowedMultipliers(buses2[0], buses2[1])));
-h.print(h.stringify(getAllMultipliersOfGivenBus(buses2, 0)));
+h.print(h.stringify(getAllMultipliersOfFirst(buses2)));
