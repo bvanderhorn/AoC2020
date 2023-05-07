97b4ac01f1f84d144b76702f422d030f88267d8a
bvanderhorn <bvanderhorn@gmail.com>
2023-05-07 23:01:30 +0200

    feat: progress

--- src/14/14.ts
+++ src/14/14.ts
@@ -8,7 +8,7 @@ var applyMask = (mask: string, value: number) : number => {
     var newbin = h.dec2bin(value).split('').reverse().map((b:string, i:number) => (i >= maskR.length || mask[i] == 'X') ? b : mask[i]).reverse().join('');
     return h.bin2dec(newbin);
 }
-var program = h.read(14, "program.txt");
+var program = h.read(14, "program.txt", "ex");
 var mask: string = "";
 var mem : Member[] = [];
 for (const instruction of program) {
@@ -16,6 +16,7 @@ for (const instruction of program) {
     var v = instruction.match(/mem\[(\d+)\] \= (\d+)$/);
     if (m !== null) {
         mask = m[1];
+        h.print("mask is now", mask);
         continue;
     }
     var loc = +v[1];
@@ -24,3 +25,5 @@ for (const instruction of program) {
     else mem.push({loc: loc, value: applyMask(mask, +v[2])});
 }
 h.print("part 1: ", mem.map(m => m.value).sum());
+h.print(applyMask(mask, 0));
+h.print(mem);
