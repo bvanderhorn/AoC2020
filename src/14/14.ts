95caf8eef83ec305009d6ee4adc08bd586dd15b0
bvanderhorn <bvanderhorn@gmail.com>
2023-05-07 20:30:57 +0200

    feat: day 14 init

---
+++ src/14/14.ts
@@ -0,0 +1,26 @@
+import * as h from '../helpers';
+type Member = {
+    loc: number,
+    value: number
+}
+var applyMask = (mask: string, value: number) : number => {
+    var maskR = mask.split('').reverse();
+    var newbin = h.dec2bin(value).split('').reverse().map((b:string, i:number) => (i >= maskR.length || mask[i] == 'X') ? b : mask[i]).reverse().join('');
+    return h.bin2dec(newbin);
+}
+var program = h.read(14, "program.txt");
+var mask: string = "";
+var mem : Member[] = [];
+for (const instruction of program) {
+    var m = instruction.match(/mask = (\w+)$/);
+    var v = instruction.match(/mem\[(\d+)\] \= (\d+)$/);
+    if (m !== null) {
+        mask = m[1];
+        continue;
+    }
+    var loc = +v[1];
+    var existing = mem.findIndex(m => m.loc == loc);
+    if (existing >= 0) mem[existing].value = applyMask(mask, +v[2]);
+    else mem.push({loc: loc, value: applyMask(mask, +v[2])});
+}
+h.print("part 1: ", mem.map(m => m.value).sum());
