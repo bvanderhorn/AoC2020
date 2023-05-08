ec7456370d88453a02f18874b4458ca793d50c27
bvanderhorn <bvanderhorn@gmail.com>
2023-05-08 09:32:25 +0200

    feat: day 14 part 1

--- src/14/14.ts
+++ src/14/14.ts
@@ -5,10 +5,10 @@ type Member = {
 }
 var applyMask = (mask: string, value: number) : number => {
     var maskR = mask.split('').reverse();
-    var newbin = h.dec2bin(value).split('').reverse().map((b:string, i:number) => (i >= maskR.length || mask[i] == 'X') ? b : mask[i]).reverse().join('');
+    var newbin = h.dec2bin(value).padStart(36,'0').split('').map((b:string, i:number) => mask[i] == 'X' ? b : mask[i]).join('');
     return h.bin2dec(newbin);
 }
-var program = h.read(14, "program.txt", "ex");
+var program = h.read(14, "program.txt");
 var mask: string = "";
 var mem : Member[] = [];
 for (const instruction of program) {
@@ -16,7 +16,6 @@ for (const instruction of program) {
     var v = instruction.match(/mem\[(\d+)\] \= (\d+)$/);
     if (m !== null) {
         mask = m[1];
-        h.print("mask is now", mask);
         continue;
     }
     var loc = +v[1];
@@ -25,5 +24,3 @@ for (const instruction of program) {
     else mem.push({loc: loc, value: applyMask(mask, +v[2])});
 }
 h.print("part 1: ", mem.map(m => m.value).sum());
-h.print(applyMask(mask, 0));
-h.print(mem);
