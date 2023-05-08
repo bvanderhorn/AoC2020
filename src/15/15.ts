d7c295b615b2dd064e9aff99e9563abe2c50e87e
bvanderhorn <bvanderhorn@gmail.com>
2023-05-08 23:31:43 +0200

    feat: day 15 part 1

---
+++ src/15/15.ts
@@ -0,0 +1,23 @@
+import * as h from '../helpers';
+type Spoken = {
+    number: number,
+    indices: number[]
+}
+var numbers = h.read(15, "numbers.txt")[0].split(',').map((n:string) => +n);
+var spoken : Spoken[] = numbers.slice(0, numbers.length-1).map((n:number,i:number) => ({number: n, indices: [i]}));
+var last = numbers[numbers.length-1];
+var inOrder = numbers;
+for (const i of h.range(numbers.length, 2020)) {
+    var lastSpoken = spoken.find(s => s.number == last);
+    if (lastSpoken === undefined) {
+        spoken.push({number: last, indices: [i-1]});
+        last = 0;
+    } else {
+        lastSpoken.indices.push(i-1);
+        last = lastSpoken.indices[lastSpoken.indices.length-1] - lastSpoken.indices[lastSpoken.indices.length-2];
+    }
+    inOrder.push(last);
+}
+
+h.print("part 1: ", inOrder[2019]);
+h.write(15, 'spokenInOrder.txt', inOrder.join('\n'));
