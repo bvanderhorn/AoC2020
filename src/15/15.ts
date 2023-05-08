f54046f767fde193b5df7e4935150fcbdd68bb57
bvanderhorn <bvanderhorn@gmail.com>
2023-05-08 23:59:52 +0200

    feat: faster

--- src/15/15.ts
+++ src/15/15.ts
@@ -3,12 +3,13 @@ var part = 2;
 var numbers = h.read(15, "numbers.txt")[0].split(',').map((n:string) => +n);
 var spoken = new Map<number, number>();
 numbers.slice(0, numbers.length-1).map((n:number,i:number) => spoken.set(n, i));
-var last = numbers[numbers.length-1];
-for (const i of h.range(numbers.length, part == 1 ? 2020 : 30000000)) {
-    h.progress(i, 30000000, 100);
+var last = numbers.last();
+var i = numbers.length;
+while (i < (part == 1 ? 2020 : 30000000)) {
+    h.progress(i, 30000000-1, 10);
     var previous = spoken.get(last);
     spoken.set(last, i-1);
-    if (previous === undefined) last = 0;
-    else last= i-1- previous;
+    last = previous === undefined ? 0 : i-1 - previous;
+    i++;
 }
 h.print("part ",part,": ", last);
