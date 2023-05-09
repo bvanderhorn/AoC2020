b1a07d34f5f51f1dab33218b3c6c07b22ebda1bc
bvanderhorn <bvanderhorn@gmail.com>
2023-05-09 18:22:26 +0200

    feat: day 16 part 1

--- src/16/16.ts
+++ src/16/16.ts
@@ -11,5 +11,5 @@ h.print(tickets.slice(0,2));
 // part 1
 var allIntervals = rules.map((r:string[]) => r.slice(1)).split('-').tonum().flat();
 var combinedIntervals = h.mergeIntervals(allIntervals);
-h.print(allIntervals);
 h.print(combinedIntervals);
+h.print("part 1:", tickets.flat().filter((n:number) => !h.isInIntervals(combinedIntervals,n)).sum());
