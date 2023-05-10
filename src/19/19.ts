ff03047940ab3fdf0b07471c32964fb3cad51e92
bvanderhorn <bvanderhorn@gmail.com>
2023-05-10 09:40:19 +0200

    feat: progress

--- src/19/19.ts
+++ src/19/19.ts
@@ -1,6 +1,7 @@
 import * as h from '../helpers';
-var [rulesRaw, messages] = h.read(19, "data.txt");
-var rules = rulesRaw.match(/(\d+)\:\s+([^\n]+)/, true);
+var [rulesRaw, messages] = h.read(19, "data.txt", "ex");
+var rules = new Map<number, string>();
+rulesRaw.match(/(\d+)\:\s+([^\n]+)/, true).map((r:string[]) => rules.set(+r[0], r[1]));
 
-h.print(rules.slice(0,3));
+h.print(rules);
 h.print(messages.slice(0,3));
