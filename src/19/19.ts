6628c4c44dae8156ca0ef67974e187b13a1f5c14
bvanderhorn <bvanderhorn@gmail.com>
2023-05-11 12:07:32 +0200

    feat: progress

--- src/19/19.ts
+++ src/19/19.ts
@@ -2,7 +2,7 @@ import * as h from '../helpers';
 var [rulesRaw, messages] = h.read(19, "data.txt");
 var rules = new Map<number, string>();
 rulesRaw.match(/(\d+)\:\s+([^\n]+)/, true).map((r:string[]) => rules.set(+r[0], r[1]));
-h.print(messages.slice(0,3));
+// h.print(messages.slice(0,3));
 
 // part 1
 var getStringsFromRule = (rule:number, rules:Map<number,string>) : string[] => {
@@ -20,4 +20,14 @@ var getStringsFromRule = (rule:number, rules:Map<number,string>) : string[] => {
 }
 
 var strings = getStringsFromRule(0, rules);
+// h.print(strings.length);
 h.print("part 1:", messages.filter((m:string) => strings.includes(m)).length);
+
+// part 2
+var s8 = getStringsFromRule(8, rules);
+var s11 = getStringsFromRule(11, rules);
+var s42 = getStringsFromRule(42, rules);
+var s31 = getStringsFromRule(31, rules);
+h.print("42:",s42.length,"\n",s42);
+h.print("31:",s31.length,"\n",s31);
+
