29b12509c4fca4ef4aca611a7c4f803217e27a5a
bvanderhorn <bvanderhorn@gmail.com>
2023-05-14 11:19:19 +0200

    feat: progress

--- src/21/21.ts
+++ src/21/21.ts
@@ -6,3 +6,13 @@ type Line = {
 var ingredients:Line[] = h.read(21, "ingredients.txt").match(/([^(]+)\s+\(contains\s+([^)]+)\)/, true)
     .map((l:string[]) => { return {Ingredients:l[0].split(' '), Allergens: l[1].split(', ')}});
 h.print(ingredients.slice(0,3));
+var allergens = new Map<string, string[]>();
+for (const line of ingredients) {
+    for (const allergen of line.Allergens) {
+        var ing = allergens.get(allergen);
+        if (ing != undefined) allergens.set(allergen, ing.intersect(line.Ingredients));
+        else allergens.set(allergen, line.Ingredients);
+    }
+}
+
+h.print(allergens);
