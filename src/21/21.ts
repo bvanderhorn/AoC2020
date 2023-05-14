8909c2522a85f6ea0411f74711b78dc4c559ac7f
bvanderhorn <bvanderhorn@gmail.com>
2023-05-14 11:13:53 +0200

    feat: progress

--- src/21/21.ts
+++ src/21/21.ts
@@ -1,2 +1,8 @@
 import * as h from '../helpers';
-var ingredients = h.read(21, "ingredients.txt");
+type Line = {
+    Ingredients: string[],
+    Allergens: string[]
+}
+var ingredients:Line[] = h.read(21, "ingredients.txt").match(/([^(]+)\s+\(contains\s+([^)]+)\)/, true)
+    .map((l:string[]) => { return {Ingredients:l[0].split(' '), Allergens: l[1].split(', ')}});
+h.print(ingredients.slice(0,3));
